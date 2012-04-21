d77e9bbc58cf8f677db56decba016d898c31a6ab
Kenichiro Nishioka <nishioka@graco.c.u-tokyo.ac.jp>
2012-04-21 09:32:25 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# What's this
+International Space Appsで作成したアプリ.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NasaHackathon::Application.config.secret_token = 'dd81eb0da3135e3751049d18e47cf2f2a248639855943002c38e1be11dc33ba79c06676ef245cf6a16660db1a4f080d34b3233b753bb0010bd6b8a0f7372fc95'
