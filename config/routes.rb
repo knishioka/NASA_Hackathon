0239a5be5d85ff65eeb6a637f44508b40c08d85f
Kenichiro Nishioka <nishioka@graco.c.u-tokyo.ac.jp>
2012-04-22 04:36:55 +0900

    added how to use api

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ NasaHackathon::Application.routes.draw do
   match "contact", :to => "home#contact", :as => :contact, :via => :get
   match "crater_quiz", :to => "home#crater_quiz", :as => :crater_quiz, :via => :get
   match "crater_quiz_result/:id", :to => "home#crater_quiz_result", :as => :crater_quiz_result, :via => :get
+  match "crater_api", :to => "home#crater_api", :as => :crater_api, :via => :get
   
   match "api/sphere", :to => "api#sphere"
   match "api/crater", :to => "api#crater"  
