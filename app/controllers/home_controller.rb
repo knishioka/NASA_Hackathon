3612e6bc731c309aaf715b572d350eef61b45ddc
Kenichiro Nishioka <nishioka@graco.c.u-tokyo.ac.jp>
2012-04-22 04:57:02 +0900

    modified top message

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class HomeController < ApplicationController
   def index
   end
@@ -7,6 +8,10 @@ class HomeController < ApplicationController
   end
 
   def crater_match_result
+    if params[:selecte_crater].nil? or params[:selected_crater] < 5
+      redirect_to crater_match_path, notice: "5つ全てを選択してください"
+      return
+    end
     sphere_id = Crater.where(:id => params[:selected_crater].values).pluck(:sphere_id).sample
     @sphere = Sphere.find(sphere_id)
   end
