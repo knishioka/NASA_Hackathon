6f5b5e013acf2fccf358a21e0c7bbefd80a7003c
Kenichiro Nishioka <nishioka@graco.c.u-tokyo.ac.jp>
2012-04-21 14:32:55 +0900

    created api base

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,8 @@
 class ApiController < ApplicationController
+  def sphere
+    render :json => {:size => 0}
+  end
+  def crater
+    render :json => { :name => "aa", :image_url => "http://"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 NasaHackathon::Application.routes.draw do
   root :to => "home#index", :as => :get
+
+  match "api/sphere", :to => "api#sphere"
+  match "api/crater", :to => "api#crater"
 end
