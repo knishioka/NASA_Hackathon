7de3f6fdb79568f348d62950815bef031920dfbb
Makoto Sato <sato@sato-air-3.local>
2012-04-23 11:29:34 +0900

    added :callback => params[:callback] for JSONP

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,23 +2,23 @@ class ApiController < ApplicationController
 
   def sphere
     sphere = Sphere.find(params[:sphere_id])
-    render :json => sphere.to_json(:except => [:created_at, :updated_at])
+    render :json => sphere.to_json(:except => [:created_at, :updated_at]), :callback => params[:callback]
   end
 
   def crater
     crater = Crater.find(params[:crater_id])
-    render :json => crater.to_json(:except => [:created_at, :updated_at])
+    render :json => crater.to_json(:except => [:created_at, :updated_at]), :callback => params[:callback]
   end
 
   def sphere_list
     @spheres = Sphere.select("id, name")
-    render :json => @spheres
+    render :json => @spheres, :callback => params[:callback]
   end
 
   def crater_list
     @craters = Crater.where(:sphere_id => params[:sphere_id]).offset(params[:offset])
       .select("id, name, image_url, latitude, longitude").limit(params[:count] ? params[:count] : 20)
-    render :json => @craters
+    render :json => @craters, :callback => params[:callback]
   end
 
 end
